f540808d8a8ae50c8dd47d966f4967e1935725a1
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-15 02:16:18 +0430

    Solve the thid slution

---
+++ Fancy.rb
@@ -0,0 +1,18 @@
+# Blanket patterns fancy solution
+# Similar to slotion 3
+# change character to halfway point
+# reverse direction after halfway point
+
+color = "=|/|/|=|/|"
+lines = 20
+halfway = (lines/2).floor
+
+color_array = color.split('')
+1.upto(halfway) do |i|
+    first = color_array.shift
+    color_array << first
+    puts color_array.join
+end
+
+# Swap the character and output the result
+color_array.length.times

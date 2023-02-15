fab0ffb5354fcadc1efde7b4aea0be33946629e0
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-16 00:28:32 +0430

    Use find method

--- find_method.rb
+++ find_method.rb
@@ -1,2 +1,10 @@
 num = (1..10).find {|n| n==5}
 puts num
+
+fruits = ['ban', 'appleddd']
+fruits.find {|fruit| fruit.length > 5}
+puts fruits
+
+pentry = {'banana' => 0, 'apple' => 2}
+pentry.detect {|k,v| v==0}
+puts pentry
